2afcc3a0585c5d546b0caf47b25d41e20532db5c
Janybek <best.joni.547545@gmail.com>
2023-01-06 16:17:45 +0600

    create header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,21 @@
 import Search from '../ui/Search/Search'
 import styles from './Header.module.scss'
+import Profile from './Profile'
 
 const Header = () => {
   return (
     <div className={styles.header}>
-      <a href='/'>
-        <img src='' alt='' />
-      </a>
-      <Search />
+      <div>
+        <a href='/'>
+          <img
+            src='https://www.freepnglogos.com/uploads/netflix-logo-0.png'
+            alt='Netflix'
+            height='35'
+            width='112'
+          />
+        </a>
+        <Search />
+      </div>
       <Profile />
     </div>
   )
